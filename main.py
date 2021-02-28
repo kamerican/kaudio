6efc374d4f979dd5c86998f58d7f634709f31a7b
kamerican <44129995+kamerican@users.noreply.github.com>
2021-02-27 18:26:43 -0600

    restart with non-realtime analysis

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+import sys
+import argparse
+import time
+from pathlib import Path
+
 # from PySide6 import QtWidgets
 # import numpy as np
 # from pyqtgraph.Qt import QtGui, QtCore, QtWidgets
 from pyqtgraph.Qt import mkQApp
 # import pyqtgraph as pg
 
-import struct
-import pyaudio
+# import struct
+# import pyaudio
 # from scipy.fftpack import fft
 
-import sys
+
 # import time
 # from kaudio.audio import AudioListener
-from kaudio.controller import Controller
-import argparse
+# from kaudio.controller import Controller
 
-import time
+
+working_dir = Path('e:/data/kaudio')
+test_file = working_dir / 'HELLO UJUNG EP3.wav'
+
+
+
+
+
+
 
 
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--list-devices', action='store_true')
+    # parser.add_argument('--list-devices', action='store_true')
     
     
     # parser.add_argument('--device', type=int, default=None, dest='device',
@@ -36,24 +48,14 @@ def parse_args():
     # parser.add_argument('--sleep_between_frames', dest='sleep_between_frames', action='store_true',
     #                     help='when true process sleeps between frames to reduce CPU usage (recommended for low update rates)')
     return parser.parse_args()
-def list_all_audio_devices():
-    pa = pyaudio.PyAudio()
-    for id in range(pa.get_device_count()):
-        dev_dict = pa.get_device_info_by_index(id)
-        for key, value in dev_dict.items():
-            print(key, value)
-        print('\n')
-
-
-
 
 if __name__ == '__main__':
     args = parse_args()
         
     if args.list_devices:
-        list_all_audio_devices()
+        # list_all_audio_devices()
+        pass
     else:
-
         # app = QtWidgets.QApplication(sys.argv)
         # app = QtWidgets.QApplication([])
         app = mkQApp()
